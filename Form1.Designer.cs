abbf9a05eb3c10ec19cc17349374ae63b3618d80
jaebins <jen2065@naver.com>
2022-03-20 12:24:40 +0900

    v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace OsuBeatmapManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but_BackDel = new System.Windows.Forms.Button();
+            this.check_Backbackup = new System.Windows.Forms.CheckBox();
+            this.But_VideoDel = new System.Windows.Forms.Button();
+            this.check_VideoBackup = new System.Windows.Forms.CheckBox();
+            this.but_BackBackup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // but_BackDel
+            // 
+            this.but_BackDel.Location = new System.Drawing.Point(12, 28);
+            this.but_BackDel.Name = "but_BackDel";
+            this.but_BackDel.Size = new System.Drawing.Size(75, 23);
+            this.but_BackDel.TabIndex = 0;
+            this.but_BackDel.Text = "배경 제거";
+            this.but_BackDel.UseVisualStyleBackColor = true;
+            this.but_BackDel.Click += new System.EventHandler(this.but_BackDel_Click);
+            // 
+            // check_Backbackup
+            // 
+            this.check_Backbackup.AutoSize = true;
+            this.check_Backbackup.Location = new System.Drawing.Point(11, 64);
+            this.check_Backbackup.Name = "check_Backbackup";
+            this.check_Backbackup.Size = new System.Drawing.Size(140, 16);
+            this.check_Backbackup.TabIndex = 1;
+            this.check_Backbackup.Text = "배경 이미지 다운로드";
+            this.check_Backbackup.UseVisualStyleBackColor = true;
+            // 
+            // But_VideoDel
+            // 
+            this.But_VideoDel.Location = new System.Drawing.Point(157, 28);
+            this.But_VideoDel.Name = "But_VideoDel";
+            this.But_VideoDel.Size = new System.Drawing.Size(81, 23);
+            this.But_VideoDel.TabIndex = 2;
+            this.But_VideoDel.Text = "동영상 제거";
+            this.But_VideoDel.UseVisualStyleBackColor = true;
+            this.But_VideoDel.Click += new System.EventHandler(this.But_VideoDel_Click);
+            // 
+            // check_VideoBackup
+            // 
+            this.check_VideoBackup.AutoSize = true;
+            this.check_VideoBackup.Location = new System.Drawing.Point(157, 64);
+            this.check_VideoBackup.Name = "check_VideoBackup";
+            this.check_VideoBackup.Size = new System.Drawing.Size(112, 16);
+            this.check_VideoBackup.TabIndex = 3;
+            this.check_VideoBackup.Text = "동영상 다운로드";
+            this.check_VideoBackup.UseVisualStyleBackColor = true;
+            // 
+            // but_BackBackup
+            // 
+            this.but_BackBackup.Location = new System.Drawing.Point(12, 86);
+            this.but_BackBackup.Name = "but_BackBackup";
+            this.but_BackBackup.Size = new System.Drawing.Size(75, 23);
+            this.but_BackBackup.TabIndex = 4;
+            this.but_BackBackup.Text = "배경 복구";
+            this.but_BackBackup.UseVisualStyleBackColor = true;
+            this.but_BackBackup.Click += new System.EventHandler(this.but_BackBackup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 139);
+            this.Controls.Add(this.but_BackBackup);
+            this.Controls.Add(this.check_VideoBackup);
+            this.Controls.Add(this.But_VideoDel);
+            this.Controls.Add(this.check_Backbackup);
+            this.Controls.Add(this.but_BackDel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but_BackDel;
+        private System.Windows.Forms.CheckBox check_Backbackup;
+        private System.Windows.Forms.Button But_VideoDel;
+        private System.Windows.Forms.CheckBox check_VideoBackup;
+        private System.Windows.Forms.Button but_BackBackup;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OsuBeatmapManagement
+{
+    public partial class Form1 : Form
+    {
+        string osuDirLoc = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\osu!\Songs";
+        string backLoc_Img = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\osuBackup_Background";
+        string backLoc_Video = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\osuBackup_Video";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void but_BackDel_Click(object sender, EventArgs e)
+        {
+
+            if (Directory.Exists(osuDirLoc))
+            {
+                DirectoryInfo osuDirInfo = new DirectoryInfo(osuDirLoc);
+
+                foreach(var item in osuDirInfo.GetDirectories())
+                {
+                    var files = Directory.GetFiles(item.FullName, "*.*", SearchOption.AllDirectories)
+                        .Where(s => s.EndsWith(".jpg") || s.EndsWith(".png"));
+                    string[] imgFiles = files.ToArray();
+
+                    for (int i = 0; i < imgFiles.Length; i++)
+                    {
+                        string fileName = GetFileName(imgFiles[i], imgFiles[i].Length - 1);
+
+                        if (check_Backbackup.Checked)
+                        {
+                            Directory.CreateDirectory(backLoc_Img + @"\" + item.Name);
+                            File.Copy(imgFiles[i], backLoc_Img + @"\" + item.Name + @"\" + fileName, true);
+                        }
+
+                        File.Delete(imgFiles[i]);
+                    }
+                }
+
+                MessageBox.Show("삭제가 완료되었습니다.");
+            }
+            else
+            {
+                MessageBox.Show("파일이 존재하지 않습니다.");
+            }
+        }
+
+        private void But_VideoDel_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(osuDirLoc))
+            {
+                DirectoryInfo osuDirInfo = new DirectoryInfo(osuDirLoc);
+
+                foreach (var item in osuDirInfo.GetDirectories())
+                {
+                    string[] files = Directory.GetFiles(item.FullName, "*.avi");
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        string fileName = GetFileName(files[i], files[i].Length - 1);
+
+                        if (check_VideoBackup.Checked)
+                        {
+                            Directory.CreateDirectory(backLoc_Video + @"\" + item.Name);
+                            File.Copy(files[i], backLoc_Video + @"\" + item.Name + @"\" + fileName, true);
+                        }
+
+                        File.Delete(files[i]);
+                    }
+                }
+
+                MessageBox.Show("삭제가 완료되었습니다.");
+            }
+            else
+            {
+                MessageBox.Show("파일이 존재하지 않습니다.");
+            }
+        }
+
+        private void but_BackBackup_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(osuDirLoc) && Directory.Exists(backLoc_Img))
+            {
+                int pasteCount = 0;
+                string[] osuDirs = Directory.GetDirectories(osuDirLoc);
+                string[] backupDirs = Directory.GetDirectories(backLoc_Img);
+                DirectoryInfo backupInfo = new DirectoryInfo(backLoc_Img);
+
+                foreach (var item in backupInfo.GetDirectories())
+                {
+                    var files = Directory.GetFiles(item.FullName, "*.*", SearchOption.AllDirectories)
+                        .Where(s => s.EndsWith(".jpg") || s.EndsWith(".png"));
+                    string[] imgFiles = files.ToArray();
+
+                    if (imgFiles.Length == osuDirLoc.Length)
+                    {
+                        MessageBox.Show("오수 맵 : " + osuDirs.Length.ToString());
+                        MessageBox.Show("복구 맵 : " + backupDirs.Length.ToString());
+                        break;
+                    }
+
+                    for (int i = 0; i < imgFiles.Length; i++)
+                    {
+                        string fileName = GetFileName(imgFiles[i], imgFiles[i].Length - 1);
+                        File.Copy(imgFiles[i], osuDirs[pasteCount] + @"\" + fileName);
+                    }
+
+                    pasteCount += 1;
+                }
+
+                MessageBox.Show("복구가 완료되었습니다.");
+            }
+            else
+            {
+                MessageBox.Show("파일이 존재하지 않습니다.");
+            }
+        }
+
+        private string GetFileName(string imgName, int len)
+        {
+            string fileName = "";
+            List<string> fileChars = new List<string>();
+
+            while (true)
+            {
+                if (imgName[len].ToString().Equals(@"\"))
+                {
+                    break;
+                }
+                fileChars.Add(imgName[len].ToString());
+                len--;
+            }
+
+            for (int k = fileChars.Count - 1; k >= 0; k--)
+            {
+                fileName += fileChars[k];
+            }
+
+            return fileName;
+        }
+
+    }
+}
